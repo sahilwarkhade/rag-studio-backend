68eb307b7dc67307e43b2291a5ffbd9e85e73e43
Sahil Warkhade <sahilwarkhade111@gmail.com>
2025-12-01 22:21:33 +0530

    cors solved

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 };
 
-app.options("/*", cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
