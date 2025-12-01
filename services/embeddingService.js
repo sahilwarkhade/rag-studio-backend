20fc6a042fb4c2b496201d9dd1df23974f831896
sahil warkhade <sahilwarkhade111@gmail.com>
2025-12-01 13:51:40 +0000

    add console log

--- services/embeddingService.js
+++ services/embeddingService.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 // export const embedding = new VertexAIEmbeddings({
 //   model: "gemini-embedding-001"
 // });
-
+console.log("API KEY ::", process.env.GEMINI_API_KEY)
 export const embedding = new GoogleGenerativeAIEmbeddings({
   apiKey:process.env.GEMINI_API_KEY,
   model:"text-embedding-004"
