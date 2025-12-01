f795427d84a409f1519a5ea119a1d7079d748704
Sahil Warkhade <sahilwarkhade111@gmail.com>
2025-12-01 21:55:02 +0530

    multer solved

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,13 +1,17 @@
-import multer from "multer"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'files/')
+    console.log({file})
+    cb(null, path.join(__dirname, "../files"));
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + "-"+file.originalname)
-  }
-})
-
-export const upload = multer({ storage: storage })
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
+  },
+});
